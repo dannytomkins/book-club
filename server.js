f7df548170b8ea53185c7316f1829ab764ed6db4
Danny Tomkins <tomkinsdan@gmail.com>
2021-07-19 11:48:44 -0500

    started server.js boilerplate. require express, set port. added scripts to start on package.json

---
+++ server.js
@@ -0,0 +1,14 @@
+// require express, back end web framework
+const express = require('express')
+
+// initialize app variable with express
+const app = express()
+
+// test
+app.get('/', (req, res) => res.send('API Running'))
+
+// looks for an environment variable named PORT, when deployed to Heroku, that is where it will get the port number. OR locally it will run on port 5000
+const PORT = process.env.PORT || 5000;
+
+// pass in the port and a callback to console.log connection statement
+app.listen(PORT, () => console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`))
