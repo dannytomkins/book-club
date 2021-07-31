8c21c1a180c182ac83591598cb765c825fa2126d
Danny Tomkins <tomkinsdan@gmail.com>
2021-07-31 08:34:18 -0500

    created routes to get all post and get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -44,4 +44,40 @@ router.post(
   }
 );
 
+
+// TO DO: find post for specific club, ? Post.find() where club = club ?
+// @route   GET api/posts
+// @desc    Get all post
+// @access  Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 })
+        res.json(posts)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+})
+
+// TO DO: find post for specific club, ? Post.find() where club = club ?
+// @route   GET api/posts/:id
+// @desc    Get post by ID
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if(!post) {
+            return res.status(404).json({msg: 'Post not found.'})
+        }
+        res.json(post)
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjentId') {
+            return res.status(404).json({msg: 'Post not found.'})
+        }
+        res.status(500).send('Server error');
+      }
+})
+
 module.exports = router;
