83ea936f6ef3cb6139b6b00ca135572a3861fc1f
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-12 14:18:40 -0500

    cant forget the () behind uuid

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -5,7 +5,7 @@ import { SET_ALERT, REMOVE_ALERT } from './types';
 // Dispatch more than one action type from this function, use dispatch from thunk middleware
 export const setAlert = (msg, alertType) => (dispatch) => {
   // generate random string id with uuid
-  const id = uuid;
+  const id = uuid();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id },
