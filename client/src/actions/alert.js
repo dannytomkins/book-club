2211d7693d77320adc7a8514bf2697a51ab85fb4
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-12 11:30:57 -0500

    comment and format

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,12 +1,13 @@
 // uuid is used to generate random universal id's
-import uuid from 'uuid'
-import { SET_ALERT, REMOVE_ALERT } from "./types";
+import uuid from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // Dispatch more than one action type from this function, use dispatch from thunk middleware
-export const setAlert = (msg, alertType) => dispatch => {
-    const id = uuid.v4();
-    dispatch({
-        type: SET_ALERT,
-        payload: { msg, alertType, id }
-    })
-}
+export const setAlert = (msg, alertType) => (dispatch) => {
+  // generate random string id with uuid
+  const id = uuid.v4();
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
+  });
+};
