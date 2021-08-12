21cd9577bd8a237eec96b5176c52321213a44899
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-12 15:16:29 -0500

    created a setTimeout function to trigger REMOVE_ALERT

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -3,11 +3,13 @@ import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // Dispatch more than one action type from this function, use dispatch from thunk middleware
-export const setAlert = (msg, alertType) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
   // generate random string id with uuid
   const id = uuid();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id },
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout)
 };
