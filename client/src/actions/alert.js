6d3a0dbd3a0a5c91c7e302142534359070202811
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-12 11:28:51 -0500

    added dispatch function to alert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,5 +1,12 @@
+// uuid is used to generate random universal id's
+import uuid from 'uuid'
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 
+// Dispatch more than one action type from this function, use dispatch from thunk middleware
 export const setAlert = (msg, alertType) => dispatch => {
-
+    const id = uuid.v4();
+    dispatch({
+        type: SET_ALERT,
+        payload: { msg, alertType, id }
+    })
 }
