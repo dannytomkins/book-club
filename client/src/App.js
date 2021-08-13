7c8560914bd88d285996d71a82c27eef38674961
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-12 20:09:29 -0500

    removed brackets from import components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Navbar } from './components/layout/Navbar';
-import { Landing } from './components/layout/Landing';
-import { Login } from './components/auth/Login';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 // import provider from react redux, this connects react and redux
