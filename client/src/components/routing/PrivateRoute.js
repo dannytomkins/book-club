85a19b9d387ac6657b061aad2c692a9c0b9d8b6a
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-16 09:23:29 -0500

    created PrivateRoute component. wrote function to check if authenticated and if not send to login

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Router, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  // Pass in all props then render props.
+  // Check if isAuthenticated and is not loading, then redirect to login.
+  // OR else if user is authenticated then load whatever component is passed in with props.
+  <Route
+    {...rest}
+    render={(props) =>
+      !auth.isAuthenticated && !loading ? (
+        <Redirect to='/login' />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
