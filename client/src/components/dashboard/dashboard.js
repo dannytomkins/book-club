9c5565c2a1098837dc74e3908ae3c585fb59cbd7
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-16 13:29:38 -0500

    import connect and getCurrentProfile. added props

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,11 +1,26 @@
 // fetch all data using an action, bring it in from redux state, pass to other components
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+import PrivateRoute from '../routing/PrivateRoute';
 
-const Dashboard = (props) => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
   return <div>DASHBOARD</div>;
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
