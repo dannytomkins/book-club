ed8f3f34a9e3fe0b7fbee0b350ae5f5d3de4ca11
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-16 09:50:21 -0500

    added types GET_PROFILE and PROFILE_ERROR. started boilerplate for profile reducer, included profile in actions types

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,16 @@
+// actions to get profile, create profile, update, clear
+
+const initialState = {
+    profile: null,
+    profiles: [],
+    loading: true,
+    error: {}
+}
+
+export default function (state = initialState, action) => {
+    const { type, payload } = action
+
+    switch(type) {
+        
+    }
+}
