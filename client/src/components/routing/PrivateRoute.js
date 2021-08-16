3d79e6445b0abba26870f8a91c430ca8e9ab9065
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-16 09:28:06 -0500

    corrected typo, Router to Route

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Redirect } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
