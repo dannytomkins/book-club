e98d435c2c3ef8b9abe5a311b886367c5114f764
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-16 15:18:26 -0500

    welcome statement with user name

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -2,19 +2,33 @@
 import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner'
+import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 import PrivateRoute from '../routing/PrivateRoute';
 
-const Dashboard = ({ getCurrentProfile, auth, profile: {profile, loading} }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
-  
+
   // We dont want to load components before we get profile data from the server.
   // If the profile is null and is still loading then show spinner.
   // Else show Fragment
-  return loading && profile === null ? <Spinner /> : <Fragment> TEST </Fragment>
+  // If user exists show user name.
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome {user && user.name}
+      </p>
+    </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
@@ -23,7 +37,7 @@ Dashboard.propTypes = {
   profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
   profile: state.profile,
 });
