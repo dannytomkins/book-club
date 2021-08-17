c54c0408ff9080306be41d67243a4d3a2f1e0c9e
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-17 12:23:50 -0500

    created createProfile function

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,44 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// Create or update profile
+// pass in history object that has method push that will redirect to a client side route
+// in order to know if profile is new or editing, updating using a parameter named edit set to false by default
+export const createProfile = (FormData, history, edit = false) => async dispatch => {
+  try {
+    // since we are sending data we need to set up config object
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    // make a post request to profile, can also update with that route
+    const res = await axios.post('/api/profile', formData, config)
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    // if edit is true say profile updated, else say profile created
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'))
+
+    // if edit then stay on page, if creating then redirect
+    if(!edit) {
+      history.push('/dashboard')
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+}
