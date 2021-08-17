14af7fda82bca626c929a23e4ae7d13bc43acdfd
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-17 15:34:30 -0500

    created dashboard actions

---
+++ client/src/components/dashboard/DashboardActions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DashboardActions = () => {
+  return (
+    <div class='dash-buttons'>
+      <Link to='/edit-profile' class='btn btn-light'>
+        <i class='fas fa-user-circle text-primary'></i> Edit Profile
+      </Link>
+      <Link to='/create-club' class='btn btn-light'>
+        <i class='fab fa-black-tie text-primary'></i> Create Club
+      </Link>
+    </div>
+  );
+};
+
+export default DashboardActions;
