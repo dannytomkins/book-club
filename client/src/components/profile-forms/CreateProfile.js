5371de552b79dac1fafa3782e3df8b9698a93c08
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-17 10:16:45 -0500

    formatted

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -13,7 +13,7 @@ const CreateProfile = (props) => {
     instagram: '',
   });
 
-  const [displaySocialInputs, toggleSocialInputs] = useState(false)
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
   const { location, status, bio, twitter, facebook, linkedin, instagram } =
     formData;
@@ -28,11 +28,9 @@ const CreateProfile = (props) => {
       <small>* = required field</small>
       <form className='form'>
         <div className='form-group'>
-        <input type='text' placeholder='Status' name='status' />
+          <input type='text' placeholder='Status' name='status' />
 
-          <small className='form-text'>
-            Status. How are you?
-          </small>
+          <small className='form-text'>Status. How are you?</small>
         </div>
         <div className='form-group'>
           <input type='text' placeholder='Location' name='location' />
@@ -43,7 +41,8 @@ const CreateProfile = (props) => {
         <div className='form-group'>
           <input type='text' placeholder='* Favorites' name='favorites' />
           <small className='form-text'>
-            Please use comma separated values (eg. Anna Karenina,To Kill a Mockingbird,Emma)
+            Please use comma separated values (eg. Anna Karenina,To Kill a
+            Mockingbird,Emma)
           </small>
         </div>
         <div className='form-group'>
@@ -51,36 +50,39 @@ const CreateProfile = (props) => {
           <small className='form-text'>Tell us a little about yourself</small>
         </div>
         <div className='my-2'>
-          <button onClick={() => toggleSocialInputs(!displaySocialInputs)}type='button' className='btn btn-light'>
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type='button'
+            className='btn btn-light'
+          >
             Add Social Network Links
           </button>
           <span>Optional</span>
         </div>
 
-    {displaySocialInputs && <Fragment>
-        
-        <div className='form-group social-input'>
-          <i className='fab fa-twitter fa-2x'></i>
-          <input type='text' placeholder='Twitter URL' name='twitter' />
-        </div>
+        {displaySocialInputs && (
+          <Fragment>
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x'></i>
+              <input type='text' placeholder='Twitter URL' name='twitter' />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-facebook fa-2x'></i>
-          <input type='text' placeholder='Facebook URL' name='facebook' />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x'></i>
+              <input type='text' placeholder='Facebook URL' name='facebook' />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-linkedin fa-2x'></i>
-          <input type='text' placeholder='Linkedin URL' name='linkedin' />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-linkedin fa-2x'></i>
+              <input type='text' placeholder='Linkedin URL' name='linkedin' />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-instagram fa-2x'></i>
-          <input type='text' placeholder='Instagram URL' name='instagram' />
-        </div>
-
-        </Fragment>}
-
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x'></i>
+              <input type='text' placeholder='Instagram URL' name='instagram' />
+            </div>
+          </Fragment>
+        )}
 
         <input type='submit' className='btn btn-primary my-1' />
         <a className='btn btn-light my-1' href='dashboard.html'>
