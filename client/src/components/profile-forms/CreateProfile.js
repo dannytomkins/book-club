b46ac1a512675e9f49e80ea05f30e095ff980cae
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-17 10:29:27 -0500

    created onChange event for each field. formatted

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -18,6 +18,10 @@ const CreateProfile = (props) => {
   const { location, status, bio, twitter, facebook, linkedin, instagram } =
     formData;
 
+  // onChange, setFormData get the rest of the formData object, get field by name use as key, get the value use as value
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Create Your Profile</h1>
@@ -28,25 +32,47 @@ const CreateProfile = (props) => {
       <small>* = required field</small>
       <form className='form'>
         <div className='form-group'>
-          <input type='text' placeholder='Status' name='status' />
-
-          <small className='form-text'>Status. How are you?</small>
+          <input
+            type='text'
+            placeholder='Status'
+            name='status'
+            value={status}
+            onChange={(e) => onChange(e)}
+          />
+          <small className='form-text'>Your status. How are you?</small>
         </div>
         <div className='form-group'>
-          <input type='text' placeholder='Location' name='location' />
+          <input
+            type='text'
+            placeholder='Location'
+            name='location'
+            value={location}
+            onChange={(e) => onChange(e)}
+          />
           <small className='form-text'>
             City & state suggested (eg. Boston, MA)
           </small>
         </div>
         <div className='form-group'>
-          <input type='text' placeholder='* Favorites' name='favorites' />
+          <input
+            type='text'
+            placeholder='* Favorites'
+            name='favorites'
+            value={favorites}
+            onChange={(e) => onChange(e)}
+          />
           <small className='form-text'>
             Please use comma separated values (eg. Anna Karenina,To Kill a
             Mockingbird,Emma)
           </small>
         </div>
         <div className='form-group'>
-          <textarea placeholder='A short bio of yourself' name='bio'></textarea>
+          <textarea
+            placeholder='A short bio of yourself'
+            name='bio'
+            value={bio}
+            onChange={(e) => onChange(e)}
+          ></textarea>
           <small className='form-text'>Tell us a little about yourself</small>
         </div>
         <div className='my-2'>
@@ -64,22 +90,46 @@ const CreateProfile = (props) => {
           <Fragment>
             <div className='form-group social-input'>
               <i className='fab fa-twitter fa-2x'></i>
-              <input type='text' placeholder='Twitter URL' name='twitter' />
+              <input
+                type='text'
+                placeholder='Twitter URL'
+                name='twitter'
+                value={twitter}
+                onChange={(e) => onChange(e)}
+              />
             </div>
 
             <div className='form-group social-input'>
               <i className='fab fa-facebook fa-2x'></i>
-              <input type='text' placeholder='Facebook URL' name='facebook' />
+              <input
+                type='text'
+                placeholder='Facebook URL'
+                name='facebook'
+                value={facebook}
+                onChange={(e) => onChange(e)}
+              />
             </div>
 
             <div className='form-group social-input'>
               <i className='fab fa-linkedin fa-2x'></i>
-              <input type='text' placeholder='Linkedin URL' name='linkedin' />
+              <input
+                type='text'
+                placeholder='Linkedin URL'
+                name='linkedin'
+                value={linkedin}
+                onChange={(e) => onChange(e)}
+              />
             </div>
 
             <div className='form-group social-input'>
               <i className='fab fa-instagram fa-2x'></i>
-              <input type='text' placeholder='Instagram URL' name='instagram' />
+              <input
+                type='text'
+                placeholder='Instagram URL'
+                name='instagram'
+                value={instagram}
+                onChange={(e) => onChange(e)}
+              />
             </div>
           </Fragment>
         )}
