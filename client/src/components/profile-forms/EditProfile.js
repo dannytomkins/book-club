fc9fe282a05eddbd825ebd96bad910024675de4b
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-18 10:02:56 -0500

    created EditProfile, copied most code from CreateProfile, tweaked where needed. imported to App

---
+++ client/src/components/profile-forms/EditProfile.js
@@ -0,0 +1,203 @@
+import React, { Fragment, useState, useEffect } from 'react';
+// import withRouter to access history
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+// need getCurrentProfile as well to edit
+import { createProfile, getCurrentProfile } from '../../actions/profile';
+
+// need profile state with profile and loading
+const EditProfile = ({
+  profile: { profile, loading },
+  createProfile,
+  getCurrentProfile,
+  history,
+}) => {
+  const [formData, setFormData] = useState({
+    location: '',
+    status: '',
+    favorites: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    instagram: '',
+  });
+
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
+  useEffect(() => {
+    getCurrentProfile();
+
+    setFormData({
+      // check all form data if loading or does not exist then display empty, else display data
+      location: loading || !profile.location ? '' : profile.location,
+      status: loading || !profile.status ? '' : profile.status,
+      favorites:
+        loading || !profile.favorites ? '' : profile.favorites.join(','),
+      bio: loading || !profile.bio ? '' : profile.bio,
+      twitter: loading || !profile.social ? '' : profile.social.twitter,
+      facebook: loading || !profile.social ? '' : profile.social.facebook,
+      linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+      instagram: loading || !profile.social ? '' : profile.social.instagram,
+    });
+    // useEffect keeps reloading, so pass in the prop or condition loading to only run on loading
+  }, [loading]);
+
+  const {
+    location,
+    status,
+    favorites,
+    bio,
+    twitter,
+    facebook,
+    linkedin,
+    instagram,
+  } = formData;
+
+  // onChange, setFormData get the rest of the formData object, get field by name use as key, get the value use as value
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // include true value for edit
+    createProfile(formData, history, true);
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Create Your Profile</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Let's get some information to make your
+        profile stand out
+      </p>
+      <small>* = required field</small>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Status'
+            name='status'
+            value={status}
+            onChange={(e) => onChange(e)}
+          />
+          <small className='form-text'>Your status. How are you?</small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Location'
+            name='location'
+            value={location}
+            onChange={(e) => onChange(e)}
+          />
+          <small className='form-text'>
+            City & state suggested (eg. Boston, MA)
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='* Favorites'
+            name='favorites'
+            value={favorites}
+            onChange={(e) => onChange(e)}
+          />
+          <small className='form-text'>
+            Please use comma separated values (eg. Anna Karenina,To Kill a
+            Mockingbird,Emma)
+          </small>
+        </div>
+        <div className='form-group'>
+          <textarea
+            placeholder='A short bio of yourself'
+            name='bio'
+            value={bio}
+            onChange={(e) => onChange(e)}
+          ></textarea>
+          <small className='form-text'>Tell us a little about yourself</small>
+        </div>
+        <div className='my-2'>
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type='button'
+            className='btn btn-light'
+          >
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
+
+        {displaySocialInputs && (
+          <Fragment>
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Twitter URL'
+                name='twitter'
+                value={twitter}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Facebook URL'
+                name='facebook'
+                value={facebook}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-linkedin fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Linkedin URL'
+                name='linkedin'
+                value={linkedin}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Instagram URL'
+                name='instagram'
+                value={instagram}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+          </Fragment>
+        )}
+
+        <input type='submit' className='btn btn-primary my-1' />
+        <Link className='btn btn-light my-1' to='/dashboard'>
+          Go Back
+        </Link>
+      </form>
+    </Fragment>
+  );
+};
+
+// also bring in getCurrentProfile action and profile object
+EditProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+// need to bring in profile state
+const mapStateToProps = state => ({
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(
+  withRouter(EditProfile)
+);
