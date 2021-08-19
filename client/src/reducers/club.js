eb150a3cfdd44d8e3e57884813625abbc342633d
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-19 09:33:22 -0500

    created club reducer

---
+++ client/src/reducers/club.js
@@ -0,0 +1,20 @@
+import { GET_CLUBS_IN } from "../actions/types";
+
+const initialState = {
+    clubsIn: [],
+    loading: true,
+    error: {}
+}
+
+export default function (state = initialState, action) {
+    const { type, payload } = action
+
+    switch(type) {
+        case GET_CLUBS_IN:
+            return {
+                ...state,
+                clubsIn: payload,
+                loading: false
+            }
+    }
+}
