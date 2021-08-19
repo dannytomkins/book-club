58cf0ade794d0bc40387c0b91725a7fad108af0e
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-19 09:19:21 -0500

    created ClubsIn starter component

---
+++ client/src/components/dashboard/ClubsIn.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-router-dom'
+
+// pass in clubsIn from App.js
+const ClubsIn = ({ clubsIn }) => {
+    const myClubs = clubsIn.map(club => (
+        <td key={club._id}>
+            <td>{club.name}</td>
+            <td>{club.description}</td>
+        </td>
+    ))
+    return (
+        <Fragment>
+            <h2 className="my2">My Clubs</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th className='hide-sm'>Name</th>
+                        <th className='hide-sm'>Description</th>
+                    </tr>
+                </thead>
+                <tbody>{myClubs}</tbody>
+            </table>
+        </Fragment>
+    )
+}
+
+ClubsIn.propTypes = {
+    clubsIn: PropTypes.array.isRequired,
+}
+
+export default ClubsIn
