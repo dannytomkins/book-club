16262ac647d5758962f9925e31f3adffc3ed33c2
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-18 20:59:15 -0500

    started getCurrentClubsIn

---
+++ client/src/actions/club.js
@@ -0,0 +1,21 @@
+// import axios from 'axios';
+// import { setAlert } from './alert';
+
+// TO DO: import types
+
+// Get clubs current user is member of
+// export const getCurrentClubsIn = () => async (dispatch) => {
+//     try {
+//         const res = await axios.get('/api/clubs/members/:user')
+//
+//         dispatch({
+//             type: GET_CLUBS_IN,
+//             payload: res.data,
+//           });
+//     } catch (err) {
+//         dispatch({
+//             type: CLUBS_IN_ERROR,
+//             payload: { msg: err.response.statusText, status: err.response.status },
+//           });
+//     }
+// }
