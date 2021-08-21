45a7989a9e9a4bd76cb063004573631a585ed52c
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-21 11:51:33 -0500

    created Profiles return and map function

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileItem from './ProfileItem'
 import { getProfiles } from '../../actions/profile';
 
 const Profiles = ({getProfiles, profile: { profiles, loading }}) => {
@@ -9,7 +10,19 @@ const Profiles = ({getProfiles, profile: { profiles, loading }}) => {
       getProfiles();
   }, [])
   
-    return <div></div>;
+    return <Fragment>
+        { loading ? <Spinner /> : <Fragment>
+            <h1 className="large text-primary">Community</h1>
+            <p className="lead">
+                <i className="fab fa-connectdevelop"></i> Connect with others.
+            </p>
+            <div className="profiles">
+                {profiles.length > 0 ? (profiles.map(profile => (
+                    <ProfileItem key={profile._id} profile={profile} />
+                ))) : <h4>No profiles found...</h4>}
+            </div>
+            </Fragment>}
+    </Fragment>;
 };
 
 Profiles.propTypes = {
