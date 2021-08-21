7a583ca514419924c3fa0e16ae6b4121ed364c1d
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-21 15:48:55 -0500

    added useEffect param match to stop warning

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -14,7 +14,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     <Fragment>
