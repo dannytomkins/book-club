83cf67dc0148ed200b72f6ae729e14960d072448
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-21 13:33:06 -0500

    added connect , proptypes and useEffect

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,19 +1,30 @@
-import React, {Fragment, useEffect} from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import Spinner from '../layout/Spinner'
-import { getProfileById } from '../../actions/profile'
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById } from '../../actions/profile';
 
-const Profile = props => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Profile = ({
+  getProfileById,
+  profile: { profile, loading },
+  auth,
+  match,
+}) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  }, [getProfileById]);
+  return <div>PROFILE</div>;
+};
 
 Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
 
-}
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
 
-export default Profile
+export default connect(mapStateToProps, { getProfileById })(Profile);
