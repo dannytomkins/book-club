49bd701cde3134ed22b2db29e5ded7c0b08eb25e
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-21 15:28:54 -0500

    added edit profile button when on own profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
@@ -14,13 +15,27 @@ const Profile = ({
   useEffect(() => {
     getProfileById(match.params.id);
   }, [getProfileById]);
-  
+
   return (
     <Fragment>
-    {profile === null || loading ? <Spinner /> : <Fragment>PROFILE</Fragment>}
-  </Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to='/profiles' className='btn btn-light'>
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
+    </Fragment>
   );
-
 };
 
 Profile.propTypes = {
