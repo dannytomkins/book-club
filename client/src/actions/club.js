fb12ea9f57d0019ac54e6be02689739e9765eb95
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-22 15:24:42 -0500

    rewrote club actions

--- client/src/actions/club.js
+++ client/src/actions/club.js
@@ -1,23 +1,21 @@
-// import axios from 'axios';
-// import { setAlert } from './alert';
+import axios from 'axios';
+import { setAlert } from './alert';
 
-// import { GET_CLUBS_IN, CLUBS_IN_ERROR } from './types';
+import { GET_CLUBS, CLUB_ERROR } from './types';
 
-// // TO DO: import types
+// // Get clubs
+export const getClubs = () => async (dispatch) => {
+    try {
+        const res = await axios.get('/api/clubs')
 
-// // Get clubs current user is member of
-// export const clubsIn = () => async (dispatch) => {
-//     try {
-//         const res = await axios.get('/api/clubs/members/me')
-
-//         dispatch({
-//             type: GET_CLUBS_IN,
-//             payload: res.data,
-//           });
-//     } catch (err) {
-//         dispatch({
-//             type: CLUBS_IN_ERROR,
-//             payload: { msg: err.response.statusText, status: err.response.status },
-//           });
-//     }
-// }
+        dispatch({
+            type: GET_CLUBS,
+            payload: res.data,
+          });
+    } catch (err) {
+        dispatch({
+            type: CLUB_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status },
+          });
+    }
+}
