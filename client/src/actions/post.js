90e45b9e4af470c3bb8f5645673ca9c4ae99fb15
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-23 14:39:15 -0500

    added addLike action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -18,3 +18,20 @@ export const getPosts = () => async (dispatch) => {
     });
   }
 };
+
+// Add Like
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`)
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, like: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
