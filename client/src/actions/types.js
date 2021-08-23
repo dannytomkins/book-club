f4b8690c78756d8ca52f1697fbafa01857986169
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-23 14:38:50 -0500

    added type UPDATE_LIKES

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -18,3 +18,4 @@ export const CLUB_ERROR='CLUB_ERROR'
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const GET_POSTS = 'GET_POSTS'
 export const POST_ERROR = 'POST_ERROR'
+export const UPDATE_LIKES = 'UPDATE_LIKES'
