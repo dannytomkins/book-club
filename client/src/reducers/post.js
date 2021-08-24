3ebb34fe1875dde512fc7105d7f4bd73d9794838
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-24 13:28:59 -0500

    created addPost action and added ADD_POST case to reducer

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
+import { ADD_POST, DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -17,6 +17,12 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [...state.posts, payload],
+        loading: false
+      }
     case DELETE_POST:
       return {
         ...state,
