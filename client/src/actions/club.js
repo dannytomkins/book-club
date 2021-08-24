ece7ec9008ad8bdec7f58a3351ca392607df5810
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-24 11:16:01 -0500

    added deleteClub action, added window confirm prompt

--- client/src/actions/club.js
+++ client/src/actions/club.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_CLUBS, CLUB_ERROR } from './types';
+import { GET_CLUBS, DELETE_CLUB, CLUB_ERROR } from './types';
 
 // // Get clubs
 export const getClubs = () => async (dispatch) => {
@@ -19,3 +19,27 @@ export const getClubs = () => async (dispatch) => {
           });
     }
 }
+
+// Delete club
+export const deleteClub = (id) => async (dispatch) => {
+    if (
+        window.confirm(
+          'Are you sure you want to delete your club? This can NOT be undone!'
+        )
+      ) {
+    try {
+      const res = await axios.delete(`/api/clubs/${id}`);
+  
+      dispatch({
+        type: DELETE_CLUB,
+        payload: id
+      });
+  
+      dispatch(setAlert('Club Removed', 'success'));
+    } catch (err) {
+      dispatch({
+        type: CLUB_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  }};
