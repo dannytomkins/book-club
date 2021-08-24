7f44bc2528bff59fc6542c24a521ce3ec6a636c4
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-24 13:54:31 -0500

    import and add PostForm

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem'
+import PostForm from './PostForm';
 import { getPosts } from '../../actions/post';
 import { post } from 'request';
 
@@ -16,7 +17,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     <p className="lead">
         <i className="fas fa-user"> Welcome to the community!</i>
     </p>
-    {/* PostForm */}
+    <PostForm />
     <div className="posts">
         {posts.map(post => (
             <PostItem key={post.id} post={post} />
