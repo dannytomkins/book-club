cc5ba9ccd8d96be7400d5ad053024e0c110a3702
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-24 14:56:27 -0500

    changed type from ADD_CLUB to GET_CLUB, added createClub function

--- client/src/actions/club.js
+++ client/src/actions/club.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_CLUBS, DELETE_CLUB, CLUB_ERROR } from './types';
+import { GET_CLUBS, GET_CLUB, DELETE_CLUB, CLUB_ERROR } from './types';
 
 // // Get clubs
 export const getClubs = () => async (dispatch) => {
@@ -44,3 +44,42 @@ export const deleteClub = (id) => async (dispatch) => {
     }
   }
 };
+
+// Create or update club
+// pass in history object that has method push that will redirect to a client side route
+// in order to know if profile is new or editing, updating using a parameter named edit set to false by default
+export const createClub =
+  (formData, history, edit = false) =>
+  async (dispatch) => {
+    try {
+      // since we are sending data we need to set up config object
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      const res = await axios.post(`/api/clubs`, formData, config);
+
+      dispatch({
+        type: GET_CLUB,
+        payload: res.data,
+      });
+
+      // if edit is true say club updated, else say club created
+      dispatch(setAlert(edit ? 'Club Updated' : 'Club Created', 'success'));
+
+      // if edit then stay on page, if creating then redirect
+      if (!edit) {
+        history.push('/dashboard');
+      }
+
+      dispatch(setAlert('Club Created', 'success'));
+    } catch (err) {
+      dispatch({
+        type: CLUB_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  };
