55f5039ee8992c159e075f434d925a071eebb59d
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-25 12:31:58 -0500

    added GET_POST case in reducer

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { ADD_POST, DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
+import { ADD_POST, DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES, GET_POST } from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -17,6 +17,12 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case GET_POST:
+      return {
+        ...state,
+        posts: payload,
+        loading: false
+      }
     case ADD_POST:
       return {
         ...state,
