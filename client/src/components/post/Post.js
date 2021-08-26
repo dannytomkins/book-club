8a029d9a543c39b05803fd5fd0b312ea969052a0
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-25 19:49:37 -0500

    returning spinner or PostItem

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,29 +1,32 @@
-import React, { Fragment, useEffect } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import Spinner from '../layout/Spinner'
-import { getPost } from '../../actions/post'
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
+import { getPost } from '../../actions/post';
 
 // bring in match to get the id from the URL
 const Post = ({ getPost, post: { post, loading }, match }) => {
-    useEffect(() => {
-        getPost(match.params.id)
-    }, [getPost])
-    
-    return (
-        <div>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ducimus facilis iure sit sapiente rem voluptates nulla id deserunt minima a neque, quas provident consequuntur aliquam omnis eveniet nemo itaque.</h1>
-        </div>
-    )
-}
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  );
+};
 
 Post.propTypes = {
-    getPost: PropTypes.func.isRequired,
-    post: PropTypes.object.isRequired,
-}
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
-    post: state.post
-})
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
 
-export default connect(mapStateToProps, {getPost})(Post)
+export default connect(mapStateToProps, { getPost })(Post);
