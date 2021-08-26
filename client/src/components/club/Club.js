2c9c77d89ee3bdd39cba68b078e9dd3b82c4d1e4
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-26 08:30:57 -0500

    created club component based on post

---
+++ client/src/components/club/Club.js
@@ -0,0 +1,36 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import ClubItem from '../posts/ClubItem';
+import { getClub } from '../../actions/club';
+
+// bring in match to get the id from the URL
+const Club = ({ getClub, club: { club, loading }, match }) => {
+  useEffect(() => {
+    getCost(match.params.id);
+  }, [getClub]);
+
+  return loading || club === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+        <Link to='/clubs' className='btn'>
+            Back To Clubs
+        </Link>
+      <ClubItem club={club} />
+    </Fragment>
+  );
+};
+
+Club.propTypes = {
+  getClub: PropTypes.func.isRequired,
+  club: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  club: state.club,
+});
+
+export default connect(mapStateToProps, { getClub })(Club);
