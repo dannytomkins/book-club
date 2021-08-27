24a14576d09e890721c279f7744e592e7d32648f
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-27 13:12:05 -0500

    added ADD_COMMENT and REMOVE_COMMENT cases to reducer

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { ADD_POST, DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES, GET_POST } from '../actions/types';
+import { ADD_POST, DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES, GET_POST, ADD_COMMENT, REMOVE_COMMENT } from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -51,6 +51,22 @@ export default function (state = initialState, action) {
         ),
         loading: false,
       };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: {...state.post, comments: payload},
+        loading: false
+      }
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          // go through all comments bring back all except the one that matches payload
+          comments: state.post.comments.filter(comment => comment._id !== payload)
+        },
+        loading: false
+      }
     default:
       return state;
   }
