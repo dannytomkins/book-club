9ab58eda7e13220b62715dd2e7d7bb82765e5b78
Danny Tomkins <tomkinsdan@gmail.com>
2021-08-31 20:13:51 -0500

    created component SearchBooks

---
+++ client/src/components/book-forms/SearchBooks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SearchBooks = props => {
+    return (
+        <div>
+            SEARCH
+        </div>
+    )
+}
+
+SearchBooks.propTypes = {
+
+}
+
+export default SearchBooks
--- client/src/components/books/Books.js
+++ client/src/components/books/Books.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import SearchBooks from '../book-forms/SearchBooks';
 
-const Books = props => {
-    return (
-        <div>
-            BOOKS - under construction
-        </div>
-    )
+
+const Books = (  ) => {
+    return null ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+            <div>BOOKS - under construction</div>
+            <SearchBooks />
+        </Fragment>
+      );
 }
 
 Books.propTypes = {
 
 }
 
-export default Books
+export default connect()(Books)
