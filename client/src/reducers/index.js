4158914c8444072ee0d6b169daf329cafc94afcb
Danny Tomkins <tomkinsdan@gmail.com>
2021-09-03 13:57:46 -0500

    corrected typo

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,7 +4,7 @@ import auth from './auth';
 import profile from './profile';
 import club from './club'
 import post from './post';
-import book from '/book'
+import book from './book'
 
 export default combineReducers({
   alert,
