961ce02f49b443453994381f789872dfb521636c
Danny Tomkins <tomkinsdan@gmail.com>
2021-09-02 22:00:28 -0500

    added mapStateToProps, propTypes, and getBooks useEffect

--- client/src/components/books/Books.js
+++ client/src/components/books/Books.js
@@ -4,22 +4,31 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import SearchBooks from '../book-forms/SearchBooks';
 import BookItem from './BookItem';
+import { getBooksByTitle } from '../../actions/book';
 
 
 const Books = (  ) => {
+  useEffect(() => {
+    getBooksByTitle()
+  }, [])
     return null ? (
         <Spinner />
       ) : (
         <Fragment>
             <div>BOOKS - under construction</div>
             <SearchBooks />
-            <BookItem />
+            <BookItem key={book.id} book={book}/>
         </Fragment>
       );
 }
 
 Books.propTypes = {
-
+  getBooksByTitle: PropTypes.func.isRequired,
+  book: PropTypes.object.isRequired,
 }
 
-export default connect()(Books)
+const mapStateToProps = state => ({
+  book: state.book
+})
+
+export default connect(mapStateToProps, { getBooksByTitle })(Books)
