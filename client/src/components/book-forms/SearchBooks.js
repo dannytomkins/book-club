b0a6dd2a1d6b88f8c1b490a3f04c676831ec5fa9
Danny Tomkins <tomkinsdan@gmail.com>
2021-09-04 14:48:21 -0500

    tweaked SearchBooks html

--- client/src/components/book-forms/SearchBooks.js
+++ client/src/components/book-forms/SearchBooks.js
@@ -1,21 +1,35 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-const SearchBooks = ({onSubmit, onChange, term}) => {
-    return (
-        <form onSubmit={onSubmit}>
-        <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Search by title." aria-label="Title" aria-describedby="basic-addon2" value={term} onChange={(e) => onChange(e)} />
-                <div className="input-group-append">
-                    <button type="submit" className="btn btn-primary">Search</button>
-                </div>
+const SearchBooks = ({ onSubmit, onChange, term }) => {
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Books</h1>
+      <p className='lead'>
+        <i className='fas fa-book'></i> Search and Browse Books
+      </p>
+      <form className='form' onSubmit={onSubmit}>
+        <div className='input-group mb-3'>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Search by title'
+            aria-label='Title'
+            aria-describedby='basic-addon2'
+            value={term}
+            onChange={(e) => onChange(e)}
+          />
+          <div className='input-group-btn'>
+            <button type='submit' className='btn btn-primary'>
+            <i className='fas fa-search'></i> Search
+            </button>
+          </div>
         </div>
-        </form>
-    )
-}
+      </form>
+    </Fragment>
+  );
+};
 
-SearchBooks.propTypes = {
+SearchBooks.propTypes = {};
 
-}
-
-export default SearchBooks
+export default SearchBooks;
