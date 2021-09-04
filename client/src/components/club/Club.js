66f2bf451762547c64f0945a626b7e2aea77bcbe
Danny Tomkins <tomkinsdan@gmail.com>
2021-09-04 14:02:03 -0500

    added getPostsByClub action to Club, imported proptypes and other deps added map function for PostItems

--- client/src/components/club/Club.js
+++ client/src/components/club/Club.js
@@ -5,13 +5,19 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ClubItem from '../clubs/ClubItem';
 import PostForm from '../posts/PostForm';
+import PostItem from '../posts/PostItem';
 import { getClub } from '../../actions/club';
+import { getPostsByClub } from '../../actions/post';
 
 // bring in match to get the id from the URL
-const Club = ({ getClub, club: { club, loading }, match }) => {
+const Club = ({ getClub, getPostsByClub, club: { club, loading }, post: { posts }, match }) => {
   useEffect(() => {
     getClub(match.params.id);
   }, [getClub]);
+
+  useEffect(() => {
+    getPostsByClub(match.params.id);
+  }, [getPostsByClub]);
 
   return loading || club === null ? (
     <Spinner />
@@ -22,17 +28,25 @@ const Club = ({ getClub, club: { club, loading }, match }) => {
         </Link>
       <ClubItem club={club} showActions={true}/>
       <PostForm club={club }/>
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={post.id} post={post} />
+        ))}
+      </div>
     </Fragment>
   );
 };
 
 Club.propTypes = {
   getClub: PropTypes.func.isRequired,
+  getPostsByClub: PropTypes.func.isRequired,
   club: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   club: state.club,
+  post: state.post,
 });
 
-export default connect(mapStateToProps, { getClub })(Club);
+export default connect(mapStateToProps, { getClub, getPostsByClub })(Club);
