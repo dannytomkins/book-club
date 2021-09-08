58d73ea2d877527f1033fa833ed42ccb743332db
Danny Tomkins <tomkinsdan@gmail.com>
2021-09-08 16:03:07 -0500

    formatted

--- models/Club.js
+++ models/Club.js
@@ -2,39 +2,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ClubSchema = new Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  creator: {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
     },
-    description: {
-        type: String,
+  },
+  members: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
     },
-    creator: {
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: 'users',
-    }
-   },
-    members: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users',
-        }
-    }
-    ],
-    admins: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users',
-        }
-    }
-    ],
-    founded: {
-        type: Date,
-        default: Date.now,
-    }
-})
+  ],
+  admins: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  founded: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = Club = mongoose.model('club', ClubSchema)
+module.exports = Club = mongoose.model('club', ClubSchema);
