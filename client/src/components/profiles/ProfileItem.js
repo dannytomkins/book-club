109528967aa42162f6693a9e5616931f8753ab81
Danny Tomkins <tomkinsdan@gmail.com>
2021-09-09 00:22:59 -0500

    added fave book images to profiles

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,37 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const ProfileItem = ({ profile: {
-    user: {_id, name, avatar },
+const ProfileItem = ({
+  profile: {
+    user: { _id, name, avatar },
     status,
     location,
-    favorites
-}}) => {
-    return (
-        <div className="profile bg-light">
-            <img src={avatar} alt="" className="round-img" />
-            <div>
-                <h2>{name}</h2>
-                <p>{status && <span>{status}</span>}</p>
-                <p className="my-1">{location && <span>{location}</span>}</p>
-                <Link to={`profile/${_id}`} className='btn btn-primary'>
-                    View Profile
-                </Link>
-            </div>
-            <ul>
-                {favorites.slice(0,5).map((book, _id) => (
-                    <li key={_id} className="text-primary">
-                        <i className="fas fa-bookmark" /> {book.title}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+    favorites,
+  },
+}) => {
+  return (
+    <div className='profile bg-light'>
+      <img src={avatar} alt='' className='round-img' />
+      <div>
+        <h2>{name}</h2>
+        <p>{status && <span>{status}</span>}</p>
+        <p className='my-1'>{location && <span>{location}</span>}</p>
+        <Link to={`profile/${_id}`} className='btn btn-primary'>
+          View Profile
+        </Link>
+      </div>
+      <ul>
+        {favorites.slice(0, 5).map((book, _id) => (
+          <li key={_id} className='text-primary'>
+            {' '}
+            <img style={{height: "30px", width: "auto"}}
+              src={
+                book.thumbnail
+                  ? book.thumbnail
+                  : <i className='fas fa-bookmark' />
+              }
+              alt=''
+            />
+            {' '+book.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 ProfileItem.propTypes = {
-    profile: PropTypes.object.isRequired,
-}
+  profile: PropTypes.object.isRequired,
+};
 
-export default ProfileItem
+export default ProfileItem;
