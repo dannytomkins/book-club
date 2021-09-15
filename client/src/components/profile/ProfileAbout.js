ec3d86e156748ea370b6c8144ba91071681ec977
Danny Tomkins <tomkinsdan@gmail.com>
2021-09-14 22:03:08 -0500

    favorites now display on single profile page, no longer crashes

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -2,6 +2,7 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
+// @TODO: change css class from skills to favorites
 const ProfileAbout = ({
   profile: {
     bio,
@@ -9,27 +10,38 @@ const ProfileAbout = ({
     user: { name },
   },
 }) => {
-    return (
-  <div class='profile-about bg-light p-2'>
-    {bio && (
-      <Fragment>
-        <h2 class='text-primary'>About Me</h2>
-        <p>{bio}</p>
-        <div class='line'></div>
-      </Fragment>
-    )}
+  return (
+    <div class='profile-about bg-light p-2'>
+      {bio && (
+        <Fragment>
+          <h2 class='text-primary'>About Me</h2>
+          <p>{bio}</p>
+          <div class='line'></div>
+        </Fragment>
+      )}
 
-    <h2 class='text-primary'>Favorites</h2>
-    <div class='skills'>
-        {favorites.slice(0,19).map((favorite, index) => (
-            <div key={index} className="p-1">
-                <i className="fas fa-bookmark" /> {favorite}
-            </div>
+      <h2 class='text-primary'>Favorites</h2>
+      <div class='skills'>
+        {favorites.slice(0, 19).map((book, _id) => (
+          <div key={_id} className='p-1'>
+            <img
+              style={{ height: '30px', width: 'auto' }}
+              src={
+                book.thumbnail ? (
+                  book.thumbnail
+                ) : (
+                  <i className='fas fa-bookmark' />
+                )
+              }
+              alt=''
+            />{' '}
+            {' '+book.title}
+          </div>
         ))}
+      </div>
     </div>
-  </div>
-    )
-    }
+  );
+};
 
 ProfileAbout.propTypes = {
   profile: PropTypes.object.isRequired,
