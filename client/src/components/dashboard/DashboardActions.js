3bd501abb63a190d29b166ad36f803b553fb5b9d
Danny Tomkins <tomkinsdan@gmail.com>
2021-11-26 18:43:44 -0600

    changed edit profile icon

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -5,7 +5,7 @@ const DashboardActions = () => {
   return (
     <div class='dash-buttons'>
       <Link to='/edit-profile' class='btn btn-light'>
-        <i class='fas fa-user-circle text-primary'></i> Edit Profile
+        <i class='fas fa-user-cog text-primary'></i> Edit Profile
       </Link>
       <Link to='/create-club' class='btn btn-light'>
         <i class='fas fa-users text-primary'></i> Create Club
